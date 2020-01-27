92828b1ee265078b46a3bbadc75a541ceb3b58e9
Nathan Ramsay-Vejlens <nathan.ramsay-vejlens@polymtl.ca>
2020-01-27 16:28:25 -0500

    p1.cpp repond aux criteres

--- tp/tp3/pb1/p1.cpp
+++ tp/tp3/pb1/p1.cpp
@@ -1,13 +1,36 @@
+/*
+    Date:       27-01-2020
+    Authors:    David Saikali, 2015144
+                Nathan Ramsay-Vejlens 1989944
+    File name:  p1.cpp
+*/
+
+/*
+    IO description: 
+    The output LED is connected to PORTC, specifically C1 and C2, using a wire we prepared.
+    The input is the interrupt button found on the motherboard. It is connected to PORTD through the motherboard itself.
+
+    General description:
+    The LED lights up in red and slowly decreases until off in 3 seconds. Same for green color.
+*/
+
 #define F_CPU 8000000UL
-#include <util/delay.h>
+
 #include <avr/io.h>
+#include <util/delay.h>
+
+#define F_CPU 8000000UL
+
 
 const int COLOR_GREEN = 0b01;
 const int COLOR_RED = 0b10;
 const int COLOR_NONE = 0b00;
 
-const int PERIOD = 1000;   //in microseconds (us)
-const int RUN_TIME = 3000; //in miliseconds (s)
+const int OUTPUT_PORT = 0xff;
+const int INPUT_PORT = 0x00;
+
+const int PERIOD = 1000;      //in us (microseconds)
+const int RUN_TIME = 3000; //in ms (miliseconds)
 
 void delay(int nombre)
 {
@@ -17,57 +40,38 @@ void delay(int nombre)
     }
 }
 
-void trial_1()
-{
-
-    DDRC = 0xff;                                          // PORT D is in output mode
-    int loopIterations = RUN_TIME / PERIOD * 1000;        //3000
-    int ratioIterationToPeriod = loopIterations / PERIOD; //3
-    int delayTime;
-    int a = loopIterations;
-    for (;;)
-    {
-        for (int i = 3000; i > 0; i--)
-        {
-            delayTime = a / ratioIterationToPeriod;
-            PORTC = COLOR_GREEN;
-            delay(delayTime);
-
-            PORTC = COLOR_NONE;
-            delay(PERIOD - delayTime);
-            a--;
-        }
-    }
-}
-
-void trial_2()
-{
-    for (;;)
-    {
-        DDRC = 0xff;                                          // PORT D is in output mode
-        int loopIterations = RUN_TIME / PERIOD * 1000;        //3000
-        int ratioIterationToPeriod = loopIterations / PERIOD; //3
-
-        int delayTime = PERIOD;
-        DDRC = 0xff; // PORT D is in output mode
-        for (int i = 0; i < loopIterations; i++)
-        {
-            for (int j = 0; j < delayTime; j++)
-            {
-                PORTC = COLOR_GREEN;
-                delay(delayTime);
-            }
-
-            for (int k = 0; k < PERIOD - delayTime; k++)
-            {
-                PORTC = COLOR_NONE;
-                delay(PERIOD - delayTime);
-            }
-            delayTime = (delayTime - 1) / ratioIterationToPeriod;
-        }
-    }
-}
-
 int main()
 {
+    DDRA = OUTPUT_PORT;
+    DDRB = OUTPUT_PORT;
+    DDRC = OUTPUT_PORT;
+    DDRD = INPUT_PORT;
+
+    for(;;){
+
+        for(int i = 0; i<RUN_TIME;i++){
+            int a = floor(i/1.5);
+
+            PORTC = COLOR_RED;
+            delay(PERIOD-a);
+
+            PORTC = COLOR_NONE;
+            delay(a);
+        }
+
+
+        for(int i = 0; i<RUN_TIME;i++){
+            int a = floor(i/1.5);
+
+            PORTC = COLOR_GREEN;
+            delay(PERIOD-a);
+
+            PORTC = COLOR_NONE;
+            delay(a);
+        }
+
+    }
+
+    return 0;
 }
+
