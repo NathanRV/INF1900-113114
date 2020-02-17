fa22ed372dfafe4f659238c83af0c71aac9a0079
Nathan Ramsay-Vejlens <nathanrv98@gmail.com>
2020-02-17 10:15:16 -0500

    fichier initial tp6 et ajout fonction utile dans tp3

---
+++ tp/tp6/pb1/p1.cpp
@@ -0,0 +1,17 @@
+#define F_CPU 8000000
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+const uint8_t OUTPUT_PORT = 0xff;
+const uint8_t INPUT_PORT = 0x00;
+const uint8_t RED = 0b10;
+const uint8_t GREEN = 0b01;
+
+int main()
+{
+    DDRD = OUTPUT_PORT;
+
+    return 0;
+}
