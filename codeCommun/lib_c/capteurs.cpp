8be8e0dfb91daac786ba47a2fb8e254f5739d480
Nathan Ramsay-Vejlens <nathan.ramsay-vejlens@polymtl.ca>
2020-03-08 21:45:50 -0400

    mod fct controlerLumiereSelonIntensite retrait param can, ajout loop infinie

--- codeCommun/lib_c/capteurs.cpp
+++ codeCommun/lib_c/capteurs.cpp
@@ -4,22 +4,23 @@
  * Utilise le capteur de photoresistance afin de détecter
  * les variations d'intensite lumineuse
  * 
- * @param convertisseur objet de type can permettant la conversion 
- *                      analogique/numerique
  * @param lumiereBasse couleur de la DEL lors du seuil minimum
  * @param lumiereHaute couleur de la DEL lors du seuil maximum
  * @return void
  */
 
-void controlerLumiereSelonIntensite(can& convertisseur, uint8_t lumiereBasse, uint8_t lumiereHaute) {
+void controlerLumiereSelonIntensite(uint8_t lumiereBasse, uint8_t lumiereHaute) {
+    can convertisseur;
     uint8_t lectureCapteur = (convertisseur.lecture(0) >> DECALAGE_2BIT);
-    if(lectureCapteur > SEUIL_LUMIERE_HAUTE) {
-        PORTB = lumiereHaute;
-    } 
-    else if(lectureCapteur < SEUIL_LUMIERE_BASSE) {
-        PORTB = lumiereBasse;
-    }
-    else {
-        couleurAmbre();
+    for(;;){
+        if(lectureCapteur > SEUIL_LUMIERE_HAUTE) {
+            PORTB = lumiereHaute;
+        } 
+        else if(lectureCapteur < SEUIL_LUMIERE_BASSE) {
+            PORTB = lumiereBasse;
+        }
+        else {
+            couleurAmbre();
+        }    
     }
 }
