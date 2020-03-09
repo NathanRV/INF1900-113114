0f8c88b95b8faff516c6381367f46b9e0c4bde10
Nathan Ramsay-Vejlens <nathan.ramsay-vejlens@polymtl.ca>
2020-03-08 22:51:08 -0400

    description fct ajouté dans .h

--- codeCommun/lib_c/antirebond.h
+++ codeCommun/lib_c/antirebond.h
@@ -4,6 +4,10 @@
 #include "constantes.h"
 #include <avr/io.h> 
 
+/**
+ * Fonction antirebond
+ * @return Un booléen qui évalue si le bouton est encore enfoncé apres un délai.
+ */
 bool boutonAntiRebond(void);
 
 #endif //ANTIREBOND_H
