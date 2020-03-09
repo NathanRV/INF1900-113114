9d096ec57ae6a8deedc16ac551f906c5cffdeb7f
Nathan Ramsay-Vejlens <nathan.ramsay-vejlens@polymtl.ca>
2020-03-08 22:52:08 -0400

    description fct attendre_ms ajouté dans .h

--- codeCommun/lib_c/delay.h
+++ codeCommun/lib_c/delay.h
@@ -1,3 +1,10 @@
 #include "constantes.h"
 
+/**
+ * Permet un delai à l'état selon le temps passeé en paramètres
+ * Cette fonction rend le passage de parametres pour les delais
+ * 
+ * @param millisecondes la duree de délai en ms
+ * @return void
+ */
 void attendre_ms(uint16_t millisecondes);
