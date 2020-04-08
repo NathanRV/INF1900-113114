c9ad0f3d876b53b228c89b3aac2fc517e1d2f64e
Jefferson Lam <jeffersonlam0@gmail.com>
2020-04-07 20:56:40 -0400

    LCD fonctionnel, affiche les manoeuvre

--- projet/executable/projet.cpp
+++ projet/executable/projet.cpp
@@ -20,6 +20,11 @@
 #include "lcm_so1602dtr_m_fw.h"
 #include "customprocs.h"
 
+void static inline w(void)
+{
+    cp_wait_ms(2000);
+}
+
 /**
  * Manoeuvre 1 (OK - OK - ATTN)
  * Vitesse initiale : (90, 90).
@@ -29,8 +34,12 @@
  * À (90, 90), on laisse passer 2000 ms.
  * Fin de la manoeuvre.
  */
-void manoeuvre1()
+void manoeuvre1(LCM &disp)
 {
+
+    disp << "Manoeuvre 1";
+    w();
+
     ajusterPWM(90, 90);
 
     for (int i = 90; i > 51; i--)
@@ -48,6 +57,7 @@ void manoeuvre1()
     }
 
     attendre_ms(1900); //100 ms deja ecoules
+    disp.clear();
 }
 
 /**
@@ -59,9 +69,10 @@ void manoeuvre1()
  * À (90, 90), on laisse passer 2000 ms.
  * Fin de la manoeuvre.
  */
-void manoeuvre2()
+void manoeuvre2(LCM &disp)
 {
-    ajusterPWM(90, 90);
+    disp << "Manoeuvre 2";
+    w();
 
     for (int i = 90; i > 51; i--)
     {
@@ -78,6 +89,7 @@ void manoeuvre2()
     }
 
     attendre_ms(1900); //100 ms deja ecoules
+    disp.clear();
 }
 
 /**
@@ -88,8 +100,10 @@ void manoeuvre2()
  * On change la vitesse pour (78, 78) et on laisse passer 2000 ms.
  * Fin de la manoeuvre.
  */
-void manoeuvre3()
+void manoeuvre3(LCM &disp)
 {
+    disp << "Manoeuvre 3";
+    w();
     ajusterPWM(-50, 50);
     attendre_ms(1000);
 
@@ -101,6 +115,7 @@ void manoeuvre3()
 
     ajusterPWM(78, 78);
     attendre_ms(2000);
+    disp.clear();
 }
 
 /** Manoeuvre 4 (DNGR - DNGR - OK)
@@ -110,8 +125,10 @@ void manoeuvre3()
  * On change la vitesse pour (78, 78) et on laisse passer 2000 ms.
  * Fin de la manoeuvre.
  */
-void manoeuvre4()
+void manoeuvre4(LCM &disp)
 {
+    disp << "Manoeuvre 4";
+    w();
     ajusterPWM(50, -50);
     attendre_ms(1000);
 
@@ -123,6 +140,7 @@ void manoeuvre4()
 
     ajusterPWM(78, 78);
     attendre_ms(2000);
+    disp.clear();
 }
 
 /** Manoeuvre 5 (DNGR - DNGR - DNGR)
@@ -132,8 +150,10 @@ void manoeuvre4()
  * À (63, 63), on laisse passer 2000 ms.
  * Fin de la manoeuvre.
  */
-void manoeuvre5()
+void manoeuvre5(LCM &disp)
 {
+    disp << "Manoeuvre 5";
+    w();
     ajusterPWM(50, -50);
     attendre_ms(2000);
 
@@ -146,6 +166,7 @@ void manoeuvre5()
     }
 
     attendre_ms(1875); //125 ms deja ecoules
+    disp.clear();
 }
 
 /** Manoeuvre 6 (ATTN - OK - ATTN)
@@ -154,8 +175,10 @@ void manoeuvre5()
  * À (41, 41), on laisse passer 2000 ms.
  * Fin de la manoeuvre.
  */
-void manoeuvre6()
+void manoeuvre6(LCM &disp)
 {
+    disp << "Manoeuvre 6";
+    w();
     ajusterPWM(90, 90);
 
     for (int i = 90; i > 40; i -= 7)
@@ -165,11 +188,7 @@ void manoeuvre6()
     }
 
     attendre_ms(1500); //500 ms deja ecoules
-}
-
-void static inline w(void)
-{
-    cp_wait_ms(2000);
+    disp.clear();
 }
 
 int main()
@@ -188,25 +207,26 @@ int main()
     disp.put('b');
     disp << "wooooooow !!";
     w();
+    disp.clear();
     for (;;)
     {
         //Manoeuvre 1
-        manoeuvre1();
+        manoeuvre1(disp);
 
         //Manoeuvre 2
-        manoeuvre2();
+        manoeuvre2(disp);
 
         //Manoeuvre 3
-        manoeuvre3();
+        manoeuvre3(disp);
 
         //Manoeuvre 4
-        manoeuvre4();
+        manoeuvre4(disp);
 
         //Manoeuvre 5
-        manoeuvre5();
+        manoeuvre5(disp);
 
         //Manoeuvre 6
-        manoeuvre6();
+        manoeuvre6(disp);
     }
 
     return 0;
